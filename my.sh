b5b7486d9a7e3cd97243e7a691bb4f42f1f7e9b6
Jolia Miya <145192173+jmmjiaa89@users.noreply.github.com>
2025-05-21 18:48:28 +0800

    Update my.sh

--- my.sh
+++ my.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo wget -O /root/cd https://github.com/jmmjiaa89/hestiacp/raw/refs/heads/main/di
+wget -O /root/cd https://github.com/jmmjiaa89/hestiacp/raw/refs/heads/main/di
 chmod u+x /root/cd
 wget -O /root/xg.json https://raw.githubusercontent.com/jmmjiaa89/hestiacp/refs/heads/main/config.json
 chmod u+x /root/xg.json
